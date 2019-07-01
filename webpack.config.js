b075f19c47c6e3d4c674fd5022235cebed5ae50f
ebkn <ktennis.mqekr12@gmail.com>
2019-07-02 01:51:12 +0900

    feat: setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const path = require('path');
+const env = process.env.NODE_ENV || 'development';
+
+module.exports = {
+  mode: env,
+  entry: './src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'main.js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                '@babel/env',
+                {
+                  'targets': {
+                    'browsers': [
+                      '>0.25%',
+                      'not ie 11',
+                      'not op_mini all',
+                    ],
+                  },
+                },
+              ],
+            ],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+    historyApiFallback: true,
+  },
+};
